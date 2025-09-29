8f65259dd4c94ca160f35baa1dbcf5f27929b981
Jessica Prendi <jessica.prendi@cern.ch>
2025-09-29 12:15:09 +0200

    fixed parsing of input params in config file

--- python/scoutingcollectionntulizer_cfg.py
+++ python/scoutingcollectionntulizer_cfg.py
@@ -3,24 +3,9 @@ import FWCore.ParameterSet.Config as cms
 from FWCore.ParameterSet.VarParsing import VarParsing
 params = VarParsing('analysis')
 
-
-params.register('inputFile',
-                'file:XYZ.root',
-                VarParsing.multiplicity.singleton,
-                VarParsing.varType.string,
-                "Input file")
-
-params.register('outputFile',
-                'XYZ.root', 
-                VarParsing.multiplicity.singleton,
-                VarParsing.varType.string,
-                "Output file")
-
-params.register('numEvents',
-                -1,
-                VarParsing.multiplicity.singleton,
-                VarParsing.varType.int,
-                "Number of events to process")
+params.setDefault('inputFiles', 'file:XYZ.root')
+params.setDefault('outputFile', 'output.root')
+params.setDefault('maxEvents', 100)
 
 params.parseArguments()
 
@@ -30,9 +15,9 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
 )
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(params.numEvents))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(params.maxEvents))
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(params.inputFile)
+    fileNames = cms.untracked.vstring(params.inputFiles)
 )
 
 process.TFileService = cms.Service("TFileService",
