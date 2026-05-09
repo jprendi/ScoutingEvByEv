87a4988e1d160d9ba5931c8b0106b58026811e1e
Jessica Prendi <jessica.prendi@cern.ch>
2026-05-09 23:39:08 +0200

    generalizing the fastMatch module also to take in input arguments

--- fastMatch.cpp
+++ fastMatch.cpp
@@ -6,7 +6,10 @@
 //
 //
 // Exectution:
-// ./fastMatch
+// ./fastMatch <distrib_A_name> <file_A.root> <distrib_B_name> <file_B.root>
+//
+// Example:
+// ./fastMatch HLT hlt_output.root Prompt prompt_output.root
 
 #include <TFile.h>
 #include <TTree.h>
@@ -51,34 +54,43 @@ std::string branch_name(const std::string& var, int pdg_id) {
   // -> "PF_eta_n211"
 }
 
-int main() {
-  const char* HLT_FILE    = "hlt_output.root";
-  const char* PROMPT_FILE = "prompt_output.root";
+int main(int argc, char* argv[]) {
+  if (argc != 5) {
+    std::cerr << "Usage: " << argv[0]
+              << " <distrib_A_name> <file_A.root> <distrib_B_name> <file_B.root>\n";
+    return 1;
+  }
+
+  const std::string distrib_A = argv[1];
+  const std::string file_A    = argv[2];
+  const std::string distrib_B = argv[3];
+  const std::string file_B    = argv[4];
+
   const char* TREE_NAME   = "scoutingCollectionNtuplizer/tree";
 
   // particle types
   const std::vector<int> pdg_ids = {211, -211, 130, 22, 13, -13, 1, 2};
 
-  TFile f_hlt(HLT_FILE);
-  TFile f_prompt(PROMPT_FILE);
+  TFile f_A(file_A.c_str());
+  TFile f_B(file_B.c_str());
 
-  TTree* t_hlt    = (TTree*)f_hlt   .Get(TREE_NAME);
-  TTree* t_prompt = (TTree*)f_prompt.Get(TREE_NAME);
+  TTree* t_A = (TTree*)f_A.Get(TREE_NAME);
+  TTree* t_B = (TTree*)f_B.Get(TREE_NAME);
 
-  ULong64_t event_hlt, event_prompt;
-  t_hlt   ->SetBranchAddress("event", &event_hlt);
-  t_prompt->SetBranchAddress("event", &event_prompt);
+  ULong64_t event_A, event_B;
+  t_A->SetBranchAddress("event", &event_A);
+  t_B->SetBranchAddress("event", &event_B);
 
   // per-particle branch pointers & histograms 
   struct ParticleData {
-    // HLT
-    std::vector<float>* eta_hlt = nullptr;
-    std::vector<float>* phi_hlt = nullptr;
-    std::vector<float>* pt_hlt  = nullptr;
-    // Prompt
-    std::vector<float>* eta_prompt = nullptr;
-    std::vector<float>* phi_prompt = nullptr;
-    std::vector<float>* pt_prompt  = nullptr;
+    // A
+    std::vector<float>* eta_A = nullptr;
+    std::vector<float>* phi_A = nullptr;
+    std::vector<float>* pt_A  = nullptr;
+    // B
+    std::vector<float>* eta_B = nullptr;
+    std::vector<float>* phi_B = nullptr;
+    std::vector<float>* pt_B  = nullptr;
     // Histograms
     TH1F* h_pt_diff  = nullptr;
     TH1F* h_eta_diff = nullptr;
@@ -92,69 +104,69 @@ int main() {
     ParticleData& p = particles[pdg];
     std::string tag = std::to_string(pdg);
 
-    // Wire up HLT branches
-    t_hlt->SetBranchAddress(branch_name("eta", pdg).c_str(), &p.eta_hlt);
-    t_hlt->SetBranchAddress(branch_name("phi", pdg).c_str(), &p.phi_hlt);
-    t_hlt->SetBranchAddress(branch_name("pT",  pdg).c_str(), &p.pt_hlt);
+    // Wire up A branches
+    t_A->SetBranchAddress(branch_name("eta", pdg).c_str(), &p.eta_A);
+    t_A->SetBranchAddress(branch_name("phi", pdg).c_str(), &p.phi_A);
+    t_A->SetBranchAddress(branch_name("pT",  pdg).c_str(), &p.pt_A);
 
-    // Wire up Prompt branches
-    t_prompt->SetBranchAddress(branch_name("eta", pdg).c_str(), &p.eta_prompt);
-    t_prompt->SetBranchAddress(branch_name("phi", pdg).c_str(), &p.phi_prompt);
-    t_prompt->SetBranchAddress(branch_name("pT",  pdg).c_str(), &p.pt_prompt);
+    // Wire up B branches
+    t_B->SetBranchAddress(branch_name("eta", pdg).c_str(), &p.eta_B);
+    t_B->SetBranchAddress(branch_name("phi", pdg).c_str(), &p.phi_B);
+    t_B->SetBranchAddress(branch_name("pT",  pdg).c_str(), &p.pt_B);
 
     // Book histograms
-    p.h_pt_diff  = new TH1F(("pt_diff_"  + tag).c_str(), ("pT(Prompt-HLT) pdg="  + tag).c_str(), 100, -5,   5  );
-    p.h_eta_diff = new TH1F(("eta_diff_" + tag).c_str(), ("eta(Prompt-HLT) pdg=" + tag).c_str(), 100, -0.1, 0.1);
-    p.h_phi_diff = new TH1F(("phi_diff_" + tag).c_str(), ("phi(Prompt-HLT) pdg=" + tag).c_str(), 100, -0.1, 0.1);
+    p.h_pt_diff  = new TH1F(("pt_diff_"  + tag).c_str(), ("pT("  + distrib_B + "-" + distrib_A + ") pdg=" + tag).c_str(), 100, -5,   5  );
+    p.h_eta_diff = new TH1F(("eta_diff_" + tag).c_str(), ("eta(" + distrib_B + "-" + distrib_A + ") pdg=" + tag).c_str(), 100, -0.1, 0.1);
+    p.h_phi_diff = new TH1F(("phi_diff_" + tag).c_str(), ("phi(" + distrib_B + "-" + distrib_A + ") pdg=" + tag).c_str(), 100, -0.1, 0.1);
     p.h_dr       = new TH1F(("dr_"       + tag).c_str(), ("DeltaR pdg="           + tag).c_str(), 100,  0,   0.05);
   }
 
-  // build prompt event index
-  std::unordered_map<ULong64_t, ULong64_t> prompt_event_map;
-  ULong64_t n_prompt = t_prompt->GetEntries();
-  for (ULong64_t i = 0; i < n_prompt; i++) {
-    t_prompt->GetEntry(i);
-    prompt_event_map[event_prompt] = i;
+  // build B event index
+  std::unordered_map<ULong64_t, ULong64_t> B_event_map;
+  ULong64_t n_B = t_B->GetEntries();
+  for (ULong64_t i = 0; i < n_B; i++) {
+    t_B->GetEntry(i);
+    B_event_map[event_B] = i;
   }
 
   // main matching loop
-  ULong64_t n_hlt = t_hlt->GetEntries();
+  ULong64_t n_A = t_A->GetEntries();
 
-  for (ULong64_t i = 0; i < n_hlt; i++) {
-    t_hlt->GetEntry(i);
+  for (ULong64_t i = 0; i < n_A; i++) {
+    t_A->GetEntry(i);
 
-    if (!prompt_event_map.count(event_hlt)) continue;
-    t_prompt->GetEntry(prompt_event_map[event_hlt]);
+    if (!B_event_map.count(event_A)) continue;
+    t_B->GetEntry(B_event_map[event_A]);
 
     // loop over particle types
     for (int pdg : pdg_ids) {
       ParticleData& p = particles[pdg];
 
-      const auto& eta_h = *p.eta_hlt;
-      const auto& phi_h = *p.phi_hlt;
-      const auto& pt_h  = *p.pt_hlt;
+      const auto& eta_a = *p.eta_A;
+      const auto& phi_a = *p.phi_A;
+      const auto& pt_a  = *p.pt_A;
 
-      const auto& eta_pr = *p.eta_prompt;
-      const auto& phi_pr = *p.phi_prompt;
-      const auto& pt_pr  = *p.pt_prompt;
+      const auto& eta_b = *p.eta_B;
+      const auto& phi_b = *p.phi_B;
+      const auto& pt_b  = *p.pt_B;
 
       // Build hash vectors
-      std::vector<int> hlt_hash   (eta_h .size());
-      std::vector<int> prompt_hash(eta_pr.size());
+      std::vector<int> A_hash   (eta_a.size());
+      std::vector<int> B_hash(eta_b.size());
 
-      for (size_t j = 0; j < eta_h .size(); j++) hlt_hash   [j] = get_hash(eta_h [j], phi_h [j]);
-      for (size_t j = 0; j < eta_pr.size(); j++) prompt_hash[j] = get_hash(eta_pr[j], phi_pr[j]);
+      for (size_t j = 0; j < eta_a.size(); j++) A_hash[j] = get_hash(eta_a[j], phi_a[j]);
+      for (size_t j = 0; j < eta_b.size(); j++) B_hash[j] = get_hash(eta_b[j], phi_b[j]);
 
-      // Prompt spatial map
-      std::unordered_map<int, std::vector<int>> prompt_map;
-      for (size_t j = 0; j < prompt_hash.size(); j++)
-        prompt_map[prompt_hash[j]].push_back(j);
+      // B spatial map
+      std::unordered_map<int, std::vector<int>> B_map;
+      for (size_t j = 0; j < B_hash.size(); j++)
+        B_map[B_hash[j]].push_back(j);
 
-      std::set<int> used_prompt;
+      std::set<int> used_B;
 
-      // Match each HLT particle to its nearest prompt neighbour
-      for (size_t h = 0; h < hlt_hash.size(); h++) {
-        int hash    = hlt_hash[h];
+      // Match each A particle to its nearest B neighbour
+      for (size_t h = 0; h < A_hash.size(); h++) {
+        int hash    = A_hash[h];
         int eta_bin = hash / PHI_BINS;
         int phi_bin = hash % PHI_BINS;
 
@@ -169,22 +181,22 @@ int main() {
             int phi_n    = (phi_bin + d_phi + PHI_BINS) % PHI_BINS;
             int neighbor = eta_n * PHI_BINS + phi_n;
 
-            if (!prompt_map.count(neighbor)) continue;
+            if (!B_map.count(neighbor)) continue;
 
-            for (int p_idx : prompt_map[neighbor]) {
-              if (used_prompt.count(p_idx)) continue;
+            for (int p_idx : B_map[neighbor]) {
+              if (used_B.count(p_idx)) continue;
 
-              double dr = delta_r(eta_h[h], phi_h[h], eta_pr[p_idx], phi_pr[p_idx]);
+              double dr = delta_r(eta_a[h], phi_a[h], eta_b[p_idx], phi_b[p_idx]);
               if (dr < best_dr) { best_dr = dr; best_idx = p_idx; }
             }
           }
         }
 
         if (best_idx != -1) {
-          used_prompt.insert(best_idx);
-          p.h_pt_diff ->Fill(pt_pr [best_idx] - pt_h [h]);
-          p.h_eta_diff->Fill(eta_pr[best_idx] - eta_h[h]);
-          p.h_phi_diff->Fill(phi_pr[best_idx] - phi_h[h]);
+          used_B.insert(best_idx);
+          p.h_pt_diff ->Fill(pt_b [best_idx] - pt_a [h]);
+          p.h_eta_diff->Fill(eta_b[best_idx] - eta_a[h]);
+          p.h_phi_diff->Fill(phi_b[best_idx] - phi_a[h]);
           p.h_dr      ->Fill(best_dr);
         }
       }
@@ -192,7 +204,8 @@ int main() {
   }   // end event loop
 
   // save histograms
-  TFile fout("matching_plots.root", "RECREATE");
+  std::string out_file = "matching_" + distrib_A + "_vs_" + distrib_B + ".root";
+  TFile fout(out_file.c_str(), "RECREATE");
   for (int pdg : pdg_ids) {
     ParticleData& p = particles[pdg];
     p.h_pt_diff ->Write();
@@ -202,5 +215,6 @@ int main() {
   }
   fout.Close();
 
-  std::cout << "Plots saved to matching_plots.root" << std::endl;
+  std::cout << "Plots saved to " << out_file << std::endl;
 }
+
